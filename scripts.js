bcd57d864372c669d615e0baa6bdc56e5954389a
ks121212 <msdpoovarasan0334@gmail.com>
2023-05-06 20:09:47 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+class person{
+    constrctor(name,father,mother,education){
+        this.name=name;
+        this.father=father;
+        this.mother=mother;
+        this.education=education;
+    }
+}
+let per=new person("Kuppusamy","Balamaniyan","Selvi", "BscMathematics");
+console.log(per);
