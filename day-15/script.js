d37e091b1fd3ba295c45fa590772abd53fceef5f
ks121212 <msdpoovarasan0334@gmail.com>
2023-05-30 18:40:49 +0530

    first commit

---
+++ day-15/script.js
@@ -0,0 +1,22 @@
+// let mouse=document.getElementById("mouse");
+
+// mouse.addEventListener("mouseover", function(event){
+//     mouse.style.color="red";
+// });
+
+// mouse.addEventListener("mouseout",function(event){
+// mouse.style.color="balck";
+//});
+
+// 2.question
+
+// let get=document.getElementById("get");
+// let span=document.getElementById("spanElement");
+
+// get.addEventListener("click",function(event){
+//     span.textContent=window.prompt("Say Somthing")
+// });
+
+
+// 3.question;
+
